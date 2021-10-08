7d68abb78ad4e8a01c686499e30bc3bc01cd3236
Noé Tarbouriech <noe.tarbouriech@posteo.net>
2021-10-09 01:02:58 +0200

    style: CSS pour que ce soit joli ✨

--- resources/tests/test-board/test.js
+++ resources/tests/test-board/test.js
@@ -12,14 +12,17 @@ function generateGrid(size) {
 
 function changeValue(cell) {
     switch(cell.innerText) {
-        case "0": cell.innerText = "1";
+        case "0": cell.innerText = "1"; cell.style = "background-color:#ff6b6b";
         break;
 
-        case "1": cell.innerText = "";
+        case "1": cell.innerText = ""; cell.style = "background-color:#ffffff";
         break;
 
-        default: cell.innerText = "0";
+        default: cell.innerText = "0"; cell.style = "background-color:#ffe66d";
     }
 }
+
+// TODO: function getArray pour obtenir une array de la grille
+// Pas d'array 2D en js => arrays imbriquées
 
 generateGrid(8);
