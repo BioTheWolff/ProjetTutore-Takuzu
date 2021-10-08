2580ff7706089a2b8f7f12799a60a7bfb4ebd772
Noé Tarbouriech <noe.tarbouriech@posteo.net>
2021-10-09 00:02:46 +0200

    feat: Changer valeur avec clic

--- resources/tests/test-board/test.js
+++ resources/tests/test-board/test.js
@@ -4,9 +4,22 @@ function generateGrid(size) {
     grid.style.setProperty('--grid-size', size);
     for (i = 0; i < (size*size); i++) {
         let cell = document.createElement("div");
-        cell.innerText = ("");
+        cell.innerText = "";
+        cell.setAttribute("onclick","changeValue(this)");
         grid.appendChild(cell).className = "cell";
     };
 }
 
-generateGrid(5);
+function changeValue(cell) {
+    switch(cell.innerText) {
+        case "0": cell.innerText = "1";
+        break;
+
+        case "1": cell.innerText = "";
+        break;
+
+        default: cell.innerText = "0";
+    }
+}
+
+generateGrid(8);
