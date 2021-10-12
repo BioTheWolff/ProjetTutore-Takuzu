cee46cfd68b7b098f35e915516378abc9542c846
Fabien Zoccola <biothewolff@gmx.fr>
2021-10-12 13:13:34 +0200

    setup de la page d'accueil et des fonctionnements internes

--- public/index.php
+++ public/index.php
@@ -3,11 +3,13 @@ require_once('../App/lib/Path.php');
 require_once('../App/lib/Config.php');
 require_once('../App/lib/RenderEngine.php');
 
+
+require_once(Path::get_path("c", "APIController"));
 require_once(Path::get_path("c", "VisualController"));
+
 
 $action = $_GET['action'] ?? false;
 
-if ($action === false)
-{
-    VisualController::index();
-}
+if ($action === false) VisualController::index();
+else if (strpos($action, "api-") == 0) APIController::$action();
+else VisualController::$action();
