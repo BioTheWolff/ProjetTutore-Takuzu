4a87d589a709f30093678e2432f334d9dba5ea4e
Fabien Zoccola <biothewolff@gmx.fr>
2021-10-23 17:47:29 +0200

    =

--- public/index.php
+++ public/index.php
@@ -11,10 +11,9 @@ require_once(Path::get_path("c", "VisualController"));
 $action = $_GET['action'] ?? false;
 // on appelle la view STP FABIEN MAIS DES COM
 if ($action === false) VisualController::index();
-else if (strpos($action, "api-") == 0)
+else if (strpos($action, "api-") === 0)
 {
     $action = str_replace("api-", "", $action);
     APIController::$action();
 }
 else VisualController::$action();
-?>
