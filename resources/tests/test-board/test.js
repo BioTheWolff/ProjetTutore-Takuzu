058a2b7fec8e8c257765f9778cd55ca56af4f1c9
Noé Tarbouriech <noe.tarbouriech@posteo.net>
2021-10-23 15:33:53 +0200

    Fin des tests js

--- resources/tests/test-board/test.js
+++ resources/tests/test-board/test.js
@@ -1,28 +1,41 @@
 const grid = document.getElementById("grid");
+const size = 8;
 
-function generateGrid(size) {
-    grid.style.setProperty('--grid-size', size);
-    for (i = 0; i < (size*size); i++) {
-        let cell = document.createElement("div");
-        cell.innerText = "";
-        cell.setAttribute("onclick","changeValue(this)");
-        grid.appendChild(cell).className = "cell";
-    };
+function generateGrid() {
+  grid.style.setProperty('--grid-size', size);
+  for (let i = 0; i < (size * size); i++) {
+    let cell = document.createElement("div");
+    cell.innerText = "";
+    cell.setAttribute("onclick", "changeValue(this)");
+    grid.appendChild(cell).className = "cell c".concat(i);
+  };
 }
 
 function changeValue(cell) {
-    switch(cell.innerText) {
-        case "0": cell.innerText = "1"; cell.style = "background-color:#ff6b6b";
-        break;
+  switch (cell.innerText) {
+    case "0": cell.innerText = "1"; cell.style = "background-color:#ff6b6b"; getValues();
+      break;
 
-        case "1": cell.innerText = ""; cell.style = "background-color:#ffffff";
-        break;
+    case "1": cell.innerText = ""; cell.style = "background-color:#ffffff"; getValues();
+      break;
 
-        default: cell.innerText = "0"; cell.style = "background-color:#ffe66d";
-    }
+    default: cell.innerText = "0"; cell.style = "background-color:#ffe66d"; getValues();
+  }
 }
 
-// TODO: function getArray pour obtenir une array de la grille
-// Pas d'array 2D en js => arrays imbriquées
+function getValues() {
+  let cells = document.getElementsByClassName("cell");
+  let values = size.toString().concat(":");
+  for (let i = 0; i < size ** 2; i++) {
+    let cell_value = cells.item(i).innerText;
+    values = values.concat(cell_value == "" ? "_" : cell_value);
+  }
+  console.log(values);
+  return values;
+}
 
-generateGrid(8);
+// TODO: function qui call sendValues() après x secondes
+
+// TODO: function qui transfère les values to php avec AJAX
+
+generateGrid(size);
