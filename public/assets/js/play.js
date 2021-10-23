30de910bf8101f569c861123d0df165a7553a9a2
Fabien Zoccola <biothewolff@gmx.fr>
2021-10-24 01:42:27 +0200

    insertion de la grille recuperee + couleurs dans le css

--- public/assets/js/play.js
+++ public/assets/js/play.js
@@ -1,60 +1,94 @@
 const grid = document.getElementById("grid");
 
 function getSize() {
-  let s = window.location.href.replace(/.*size=([0-9]).*/, "");
-  if (s == "") {
-    return 8; // valeur par défaut
-  } else {
-    return parseInt(s);
-  }
+    let s = window.location.href.replace(/.*size=([0-9]).*/, "");
+    if (s === "") {
+        return 8; // valeur par défaut
+    } else {
+        return parseInt(s);
+    }
 }
 
-function getGrid(size) {
-  var xmlhttp = new XMLHttpRequest();
-  let g = "";
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      g = this.responseText;
+function downloadAndParseGrid(size) {
+    let req = new XMLHttpRequest();
+    req.open("GET", "?action=api-generate&size=" + size, true);
+    req.send();
+
+    req.onload = function () {
+        if (req.status === 200) {
+            size = req.response.split(":")[0];
+            insertInGrid(req.response.split(":")[1]);
+        }
+        else alert("Impossible de charger la grille");
     }
-  };
-  xmlhttp.open("GET", "?action=api-generate&size=" + size, true);
-  xmlhttp.send();
-  return g;
+}
+
+
+function insertInGrid(content) {
+    /** @type {string[]} **/
+    let arr = Array.from(content);
+
+    for (let cell of grid.children) {
+        switch (arr[cell.id]) {
+            case "_":
+                break;
+            case "0":
+                cell.classList.replace("empty", "zero");
+                cell.textContent = "0";
+                break;
+
+            case "1":
+                cell.classList.replace("empty", "one");
+                cell.textContent = "1";
+                break;
+        }
+    }
 }
 
 function generateEmptyGrid() {
-  grid.style.setProperty('--grid-size', size);
-  for (let i = 0; i < size ** 2; i++) {
-    let cell = document.createElement("div");
-    cell.innerText = "";
-    cell.setAttribute("onclick", "changeValue(this)");
-    grid.appendChild(cell).className = "cell c".concat(i);
-  };
+    grid.style.setProperty('--grid-size', size.toString());
+    for (let i = 0; i < size ** 2; i++) {
+        let cell = document.createElement("div");
+        cell.innerText = "";
+        cell.setAttribute("onclick", "changeValue(this)");
+        cell.id = i.toString();
+        cell.classList.add("cell", "empty")
+        grid.appendChild(cell);
+    }
 }
 
 // TODO: function qui entre les valeurs de getGrid() dans la grid vide
 
 function changeValue(cell) {
-  switch (cell.innerText) {
-    case "0": cell.innerText = "1"; cell.style = "background-color:#ff6b6b"; getValues();
-      break;
+    switch (cell.innerText) {
+        case "0":
+            cell.classList.replace("zero", "one")
+            cell.innerText = "1";
+            cell.style = "background-color:#ff6b6b";
+            break;
 
-    case "1": cell.innerText = ""; cell.style = "background-color:#ffffff"; getValues();
-      break;
+        case "1":
+            cell.classList.replace("one", "empty")
+            cell.innerText = "";
+            cell.style = "background-color:#ffffff";
+            break;
 
-    default: cell.innerText = "0"; cell.style = "background-color:#ffe66d"; getValues();
-  }
+        default:
+            cell.classList.replace("empty", "zero")
+            cell.innerText = "0";
+            cell.style = "background-color:#ffe66d";
+    }
 }
 
 function getValues() {
-  let cells = document.getElementsByClassName("cell");
-  let values = size.toString().concat(":");
-  for (let i = 0; i < size ** 2; i++) {
-    let cell_value = cells.item(i).innerText;
-    values = values.concat(cell_value == "" ? "_" : cell_value);
-  }
-  console.log(values);
-  return values;
+    let cells = document.getElementsByClassName("cell");
+    let values = size.toString().concat(":");
+    for (let i = 0; i < size ** 2; i++) {
+        let cell_value = cells.item(i).innerText;
+        values = values.concat(cell_value === "" ? "_" : cell_value);
+    }
+    console.log(values);
+    return values;
 }
 
 // TODO: function sendValues() qui transfère les values to php avec AJAX
@@ -62,6 +96,7 @@ function getValues() {
 // TODO: function qui call sendValues() après x secondes
 
 let size = getSize();
-let gridString = getGrid();
+gridString = "";
 generateEmptyGrid(size);
+downloadAndParseGrid();
 
