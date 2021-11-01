f02f61e93cc4ae95f755d37c8a46bb409ba20b9a
Fabien "BioTheWolff" Zoccola <biothewolff@gmx.fr>
2021-11-01 15:52:54 +0100

    solving grids using only multiplicity constraint

--- App/Model/GridSolver.php
+++ App/Model/GridSolver.php
@@ -29,7 +29,6 @@ class GridSolver
         $this->gap = Adapter::GAP_PHP;
     }
 
-
     /**
      * Solves a given grid
      *
@@ -43,10 +42,10 @@ class GridSolver
     }
 
 
+    // MAIN FUNCTION
+
     private function solve()
     {
-        // FIXME dummy function that fills the grid with 0s ; THIS IS A PLACEHOLDER
-
         while (!$this->filled())
         {
             foreach (['l', 'c'] as $d)
@@ -58,16 +57,32 @@ class GridSolver
 
                         if ($this->get($d, $i, $j) != $this->gap) continue;
 
+                        if (($v = $this->check_mult($d, $i, $j)) != null) $this->fill($d, $i, $j, 1-$v);
+
                     }
                 }
             }
         }
-
     }
+
+
+    // UTILS
 
     private function get(string $direction, int $i, int $j): int
     {
         return $direction == 'l' ? $this->grid[$i][$j] : $this->grid[$j][$i];
+    }
+
+    private function fill(string $direction, int $i, int $j, int $value)
+    {
+        if ($direction == 'c')
+        {
+            $temp = $i;
+            $i = $j;
+            $j = $temp;
+        }
+
+        $this->grid[$i][$j] = $value;
     }
 
     private function filled(): bool
@@ -93,4 +108,37 @@ class GridSolver
 
     }
 
+
+    // CHECKS
+
+    /**
+     * Checks whether the current gap fits one of the patterns below:
+     *
+     * using X in {0,1} and _ the gap:
+     *  - XX_
+     *  - X_X
+     *  - _XX
+     *
+     *
+     * @param string $direction the direction the program is looking at the grid with
+     * @param int $i the line
+     * @param int $j the column
+     * @return int|null the repeating char if the grid fits one of the patterns above, else null
+     */
+    private function check_mult(string $direction, int $i, int $j): ?int
+    {
+        $size = count($this->grid);
+        $c = null;
+
+        $cond = (
+            // XX_ pattern
+            ($j >= 2 && (($c = self::get($direction, $i, $j-1)) == self::get($direction, $i, $j-2))) ||
+            // X_X pattern
+            ($j >= 1 && $j < $size-1 && (($c =self::get($direction, $i, $j-1)) == self::get($direction, $i, $j+1))) ||
+            // _XX pattern
+            ($j < $size-2 && (($c = self::get($direction, $i, $j+1)) == self::get($direction, $i, $j+2)))
+        );
+
+        return $cond ? $c : null;
+    }
 }
