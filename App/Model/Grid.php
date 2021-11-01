887fd6b0503ce659aebd679efd293a1d5fd47ec9
Fabien "BioTheWolff" Zoccola <biothewolff@gmx.fr>
2021-11-01 15:29:20 +0100

    resolution stems with get and filled methods

---
+++ App/Model/Grid.php
@@ -0,0 +1,8 @@
+<?php
+
+class Grid
+{
+
+    // TODO: implement the Grid hypothesis
+
+}
--- App/Model/GridSolver.php
+++ App/Model/GridSolver.php
@@ -4,20 +4,29 @@ class GridSolver
 {
 
     /**
+     * The grid variable where in-place replacement happens
      * @var array $grid
      */
     private $grid;
 
     /**
-     * @var array $backtrace
+     * The list of hypothesis made by the program
+     * @var Grid[] $backtrace
      */
     private $backtrace;
 
+    /**
+     * The gap type.
+     * Setting it as an instance variable solves the dependency import order problem
+     * @var int $gap
+     */
+    private $gap;
 
     private function __construct(array $grid)
     {
         $this->grid = $grid;
         $this->backtrace = [];
+        $this->gap = Adapter::GAP_PHP;
     }
 
 
@@ -38,13 +47,50 @@ class GridSolver
     {
         // FIXME dummy function that fills the grid with 0s ; THIS IS A PLACEHOLDER
 
-        for ($i = 0; $i < count($this->grid); $i++)
+        while (!$this->filled())
         {
-            for ($j = 0; $j < count($this->grid); $j++)
+            foreach (['l', 'c'] as $d)
             {
-                $this->grid[$i][$j] = 0;
+                for ($i = 0; $i < count($this->grid); $i++)
+                {
+                    for ($j = 0; $j < count($this->grid); $j++)
+                    {
+
+                        if ($this->get($d, $i, $j) != $this->gap) continue;
+
+                    }
+                }
             }
         }
+
+    }
+
+    private function get(string $direction, int $i, int $j): int
+    {
+        return $direction == 'l' ? $this->grid[$i][$j] : $this->grid[$j][$i];
+    }
+
+    private function filled(): bool
+    {
+        /**
+         * @see https://stackoverflow.com/questions/4128323/in-array-and-multidimensional-array/4128377#4128377 original solution
+         * @param mixed $needle what to look for
+         * @param array $haystack where to look in
+         * @return bool if the object was found
+         */
+        function in_array_r($needle, array $haystack): bool
+        {
+            foreach ($haystack as $item) {
+                if ($item == $needle || (is_array($item) && in_array_r($needle, $item))) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return in_array_r($this->gap, $this->grid);
+
     }
 
 }
