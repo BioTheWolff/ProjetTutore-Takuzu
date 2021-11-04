1e192527a4b05b09296afa66e9873e4a518abca4
Noé Tarbouriech <noe.tarbouriech@posteo.net>
2021-11-05 00:20:52 +0100

    boutons back/forward qui marchent enfin !

--- public/assets/js/play.js
+++ public/assets/js/play.js
@@ -1,4 +1,6 @@
 const grid = document.getElementById("grid");
+const backwardBtn = document.getElementById("backward");
+const forwardBtn = document.getElementById("forward");
 
 // return grid size from url
 function getSize() {
@@ -16,7 +18,7 @@ function getSize() {
 function downloadAndParseGrid() {
     fetch("?action=api-generate&size=" + getSize())
         .then(response => response.text())
-        .then(data => setupGrid(data.split(":")[0], data.split(":")[1]))
+        .then(data => fillGrid(data.split(":")[0], data.split(":")[1], true))
         .catch((error) => alert("Impossible de charger la grille: " + error))
 }
 
@@ -34,27 +36,52 @@ function generateEmptyGrid(size) {
 }
 
 // fill-in values in the grid
-function setupGrid(size, content) {
-    generateEmptyGrid(size);
+function fillGrid(size, content, init = false) {
+    if (grid.children.length == 0) {
+        generateEmptyGrid(size);
+    }
 
     /** @type {string[]} **/
     let arr = Array.from(content);
 
-    for (let cell of grid.children) {
-        switch (arr[cell.id]) {
-            case "_":
-                break;
-            case "0":
-                cell.classList.replace("empty", "zero");
-                cell.classList.add("static");
-                cell.textContent = "0";
-                break;
-
-            case "1":
-                cell.classList.replace("empty", "one");
-                cell.classList.add("static");
-                cell.textContent = "1";
-                break;
+    // TODO: do something about statics, maybe new function for init grid or if
+    if (init == true) {
+        for (let cell of grid.children) {
+            switch (arr[cell.id]) {
+                case "_":
+                    break;
+                case "0":
+                    cell.classList.replace("empty", "zero");
+                    cell.classList.add("static");
+                    cell.textContent = "0";
+                    break;
+                case "1":
+                    cell.classList.replace("empty", "one");
+                    cell.classList.add("static");
+                    cell.textContent = "1";
+                    break;
+            }
+        }
+    } else {
+        for (let cell of grid.children) {
+            if (!cell.classList.contains("static"))
+                switch (arr[cell.id]) {
+                    case "_":
+                        cell.classList.replace("zero", "empty");
+                        cell.classList.replace("one", "empty");
+                        cell.textContent = "";
+                        break;
+                    case "0":
+                        cell.classList.replace("empty", "zero");
+                        cell.classList.replace("one", "zero");
+                        cell.textContent = "0";
+                        break;
+                    case "1":
+                        cell.classList.replace("empty", "one");
+                        cell.classList.replace("zero", "one");
+                        cell.textContent = "1";
+                        break;
+                }
         }
     }
 }
@@ -71,42 +98,47 @@ function changeValue(cell) {
     clearTimeout(timer);
     timer = setTimeout(sendValues, 3000);
 
+    currGrid = getValues();
+    backwardGrids.push(currGrid);
     switch (cell.innerText) {
         case "0":
             cell.classList.replace("zero", "one")
             cell.innerText = "1";
-            cell.style = "background-color:#ff6b6b";
             break;
 
         case "1":
             cell.classList.replace("one", "empty")
             cell.innerText = "";
-            cell.style = "background-color:#ffffff";
             break;
 
         default:
             cell.classList.replace("empty", "zero")
             cell.innerText = "0";
-            cell.style = "background-color:#ffe66d";
     }
+    backwardBtn.disabled = false;
 }
 
 // return values from the grid
 function getValues() {
-    let size = grid.style.getPropertyValue('--grid-size');
-    let values = size.toString().concat(":");
-
+    let values = "";
     for (let i = 0; i < size ** 2; i++) {
         let cell_value = (cells.item(i) == null ? "" : cells.item(i).innerText);
         values = values.concat(cell_value === "" ? "_" : cell_value);
     }
-    console.log("\n" + "📨 GRID:   " + values);
     return values;
+}
+
+// return gridd with values and proper formating for api
+function getGrid() {
+    let size = grid.style.getPropertyValue('--grid-size');
+    let gridStr = size.toString().concat(":").concat(getValues());
+    console.log("\n" + "📨 GRID:   " + gridStr);
+    return gridStr;
 }
 
 // send values to the php api
 function sendValues() {
-    fetch("?action=api-check&message=" + getValues())
+    fetch("?action=api-check&message=" + getGrid())
         .then(response => response.text())
         .then(data => alertWin(data))
         .catch((error) => alert("Impossible de charger la grille: " + error))
@@ -127,7 +159,6 @@ function alertWin(data) {
 // apply colors to the grid to show errors
 function highlightErrors(errors) {
     let errsplit = errors.split(":");
-    console.log(errsplit);
     if (errsplit[1] === "c") {
         for (let i = parseInt(errsplit[3]); i < size ** 2; i = i + size) {
             cells.item(i).classList.add("wrong");
@@ -139,10 +170,52 @@ function highlightErrors(errors) {
     }
 }
 
+// backward in grids history
+function backward() {
+    // add previous grid to forwardGrids
+    forwardBtn.disabled = false;
+    currGrid = getValues();
+    forwardGrids.push(currGrid);
+
+    // update currGrid by taking latest grid from backwardGrids
+    currGrid = backwardGrids.pop();
+    fillGrid(size, currGrid);
+
+    // disable btn if backward is impossible
+    if (backwardGrids.length == 0) backwardBtn.disabled = true;
+}
+
+// forward in grids history
+function forward() {
+    // add previous grid to backwardGrids
+    backwardBtn.disabled = false;
+    currGrid = getValues();
+    backwardGrids.push(currGrid);
+
+    // update currGrid by taking latest grid from backwardGrids
+    currGrid = forwardGrids.pop();
+    fillGrid(size, currGrid);
+
+    // disable btn if forward is impossible
+    if (forwardGrids.length == 0) forwardBtn.disabled = true;
+}
+
 let valuesFilled = 0;
 let cells = document.getElementsByClassName("cell");
-let timer = setTimeout(sendValues, 3000);
 let size = getSize();
+
+// timer for sending grids to api
+let timer = setTimeout(sendValues, 3000);
 clearTimeout(timer);
+
+// grid history
+let backwardGrids = [];
+let forwardGrids = [];
+let currGrid = "";
+
+// buttons disabled
+backwardBtn.disabled = true;
+forwardBtn.disabled = true;
+
 downloadAndParseGrid();
 
