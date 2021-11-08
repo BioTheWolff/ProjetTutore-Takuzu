7ca345c693a5c0ab4dabadb66d1837409b94f8a6
Hoareau Paul <paul.hoareau@etu.umontpellier.fr>
2021-11-08 09:21:05 +0100

    ajout page règle et contact

--- App/Controller/VisualController.php
+++ App/Controller/VisualController.php
@@ -9,6 +9,14 @@ class VisualController
         RenderEngine::render("index", "Accueil");
     }
 
+    public static function regle(){
+        RenderEngine::render("regle","Regle");
+    }
+
+    public static function contact(){
+        RenderEngine::render("contact","Contact");
+    }
+
     public static function play()
     {
         RenderEngine::render("play", "Jeu");
