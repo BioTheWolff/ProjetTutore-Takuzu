bcb3a70d884e807197040142091aca44773cc2fb
Hoareau Paul <paul.hoareau@etu.umontpellier.fr>
2021-11-08 09:21:05 +0100

    ajout page règle et contact

--- App/view/content/contact.php
+++ App/view/content/contact.php
@@ -1,1 +1,18 @@
-<?php
+<h2>Pour nous contacter, c'est simple !</h2>
+<div class="contact">
+    <div class="information" id="infos">
+        <img class="icon" src="assets/img/num_icon.png">
+        <p>0766613041</p>
+    </div>
+    <div class="information">
+        <img class="icon" src="assets/img/email_icon.png">
+        <p>paul.hoareau@etu.umontpellier.fr</p>
+    </div>
+</div>
+<div class="contact" id="second">
+    <div class="information">
+        <img class="icon" src="assets/img/adress_icon.png">
+        <p>99, Avenue d’Occitanie, 34296 MONTPELLIER</p>
+    </div>
+</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.616549108763!2d3.848475551313522!3d43.635339261500576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6aec3fc4072d7%3A0x1a1585a408bcfed4!2sIUT%20Montpellier!5e0!3m2!1sfr!2sfr!4v1636338618131!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
