53923494df91897807b64d5b9184a4ec62c91548
Suzanne Robert <suzanne.robert@etu.umontpellier.fr>
2021-11-22 16:35:57 +0100

    Delete the logo in the header

--- App/view/_header.php
+++ App/view/_header.php
@@ -1,7 +1,7 @@
 <nav>
     <div class="navbar">
         <ul class="navbar-menu">
-            <img id="yuzu" src="assets/img/Yuzu2.png" alt="logo" onclick="goHome()">
+            <button class="pages" onclick="goHome()">Accueil</button>
             <button class="pages" onclick="goRegle()">Règles</button>
             <button class="pages" onclick="goContact()">Contact</button>
         </ul>
