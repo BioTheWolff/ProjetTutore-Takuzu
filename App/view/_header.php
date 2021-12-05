77b543e0bb1c9a0becacc70aedd92044823fe33c
Hoareau Paul <paul.hoareau@etu.umontpellier.fr>
2021-12-05 15:22:01 +0100

    header & footer fait

--- App/view/_header.php
+++ App/view/_header.php
@@ -1,7 +1,9 @@
 <nav>
     <div class="navbar">
         <div class="navbar-menu">
-            <img id="yuzu" src="assets/img/Yuzu.png" alt="logo" onclick="goHome()">
+            <div class="company" onclick="goHome()">
+                <h1>Takuzu</h1>
+            </div>
             <div class="nav-buttons">
                 <button class="pages" onclick="goRegle()">Règles</button>
                 <button class="pages" onclick="goContact()">Contact</button>
